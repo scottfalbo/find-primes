d1b7de9e5cabb051fa8eef990a2c708b46e047ee
Scott Falbo <scottfalboart@gmail.com>
2021-10-24 20:52:19 -0700

    Added IsPrime method

--- FindPrimes/Program.cs
+++ FindPrimes/Program.cs
@@ -9,5 +9,21 @@ namespace FindPrimes
             Console.WriteLine("Hello World!");
         }
 
+        static bool IsPrime(int n)
+        {
+            if (n < 2)
+            {
+                return false;
+            }
+
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
