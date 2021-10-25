5b559ba23a90276ce2429d5afb939ac78d8612bb
Scott Falbo <scottfalboart@gmail.com>
2021-10-24 21:47:54 -0700

    Tested PrimesBeforeN

--- FindPrimesTests/UnitTest1.cs
+++ FindPrimesTests/UnitTest1.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using FindPrimes;
+using System.Collections.Generic;
 
 namespace FindPrimesTests
 {
@@ -32,5 +33,26 @@ namespace FindPrimesTests
             int result = FindPrimes.FindPrimes.TotalPrimesBeforeN(n);
             Assert.Equal(expected, result);
         }
+
+        [Theory]
+        [MemberData(nameof(PrimeListData))]
+
+        public void PrimesBeforeNCanSuccessfullyReturnAListOfPrimeValues(int n, List<int> expected)
+        {
+            List<int> result = FindPrimes.FindPrimes.PrimesBeforeN(n);
+            result.Reverse();
+            Assert.Equal(expected, result);
+        }
+        /// <summary>
+        /// Helper object for PrimesBeforeN tests
+        /// </summary>
+        public static IEnumerable<object[]> PrimeListData =>
+            new List<object[]>
+            {
+                        new object[] { 5, new List<int>() { 2, 3} },
+                        new object[] { 12, new List<int>() { 2, 3, 5, 7, 11} },
+                        new object[] { 100, new List<int>() { 2, 3, 5, 7, 11, 13, 17, 19, 23,
+                            29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97} }
+            };
     }
 }
