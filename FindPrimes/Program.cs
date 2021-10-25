8f2906526cdeeba98ed83af16d87909a3c86a423
Scott Falbo <scottfalboart@gmail.com>
2021-10-24 21:58:57 -0700

    Added simple console UI in Program.cs

--- FindPrimes/Program.cs
+++ FindPrimes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FindPrimes
 {
@@ -6,11 +7,24 @@ namespace FindPrimes
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Prime Finder \n");
+            int input = 0;
+            while (input < 1 || input > 1000)
+            {
+                Console.WriteLine("Enter a value between 1 and 1000");
+                input = Convert.ToInt32(Console.ReadLine());
+            }
+            int total = FindPrimes.TotalPrimesBeforeN(input);
+            List<int> primes = FindPrimes.PrimesBeforeN(input);
+            primes.Reverse();
 
+            Console.WriteLine($"\nThere are {total} prime numbers before {input} \n");
+            Console.WriteLine($"Primes before {input}");
+            foreach (int value in primes)
+            {
+                Console.Write($"{value}, ");
+            }
+            Console.WriteLine("\n \n");
         }
-
-
-
-
     }
 }
